22a1b9c0ee1f57d5268e27521909a02b29c3ed18
dgusfr <diegogustavo.fr@gmail.com>
2026-02-01 12:52:36 -0300

    feat(database): update DATABASE_URL to use environment variable for configuration feat(docker): add Dockerfile for containerized application setup

--- database/database.py
+++ database/database.py
@@ -1,8 +1,12 @@
+#
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/db_escola"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/db_escola"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
