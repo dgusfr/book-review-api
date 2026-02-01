c59a2de066f052716acdb9d60869c2794db9b9c3
dgusfr <diegogustavo.fr@gmail.com>
2026-02-01 11:14:57 -0300

    feat(models): update Estudante model with additional fields and constraints

--- database/schemas.py
+++ database/schemas.py
@@ -1,8 +1,12 @@
-from typing import List, Optional
+from typing import Optional
 from pydantic import BaseModel
 
 
-# Representa o modelo de perfil associado ao estudante. from_attributes permite criar esse schema a partir de objetos ORM (SQLAlchemy).
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
 class Perfil(BaseModel):
     id: int
     idade: int
@@ -12,43 +16,42 @@ class Perfil(BaseModel):
         from_attributes = True
 
 
-# Modelo usado para criação de um perfil, o banco gera o id automaticamente
 class PerfilCreate(BaseModel):
     idade: int
     endereco: str
 
 
-# Modleo de SAÍDA de estudante com objeto Estudante convertido de ORM para JSON.
 class Estudante(BaseModel):
     id: int
     nome: str
+    email: str
     perfil: Optional[Perfil] = None
 
     class Config:
         from_attributes = True
 
 
-# Modelo de ENTRADA para criar estudante
 class EstudanteCreate(BaseModel):
     nome: str
     email: str
+    senha: str
     perfil: PerfilCreate
 
 
-# Modelo de SAÍDA de disciplina
 class Disciplina(BaseModel):
     id: int
     nome: str
     descricao: str
 
+    class Config:
+        from_attributes = True
 
-# Modelo de ENTRADA para criar disciplina
+
 class DisciplinaCreate(BaseModel):
     nome: str
     descricao: str
 
 
-# Modelo de SAÍDA para matrícula, representa o vínculo entre estudante e disciplina usando seus ids
 class Matricula(BaseModel):
     estudante_id: int
     disciplina_id: int
@@ -57,13 +60,11 @@ class Matricula(BaseModel):
         from_attributes = True
 
 
-# Modelo de ENTRADA para criar uma matrícula.
 class MatriculaCreate(BaseModel):
     estudante_id: int
     disciplina_id: int
 
 
-# Modelo de SAÍDA de professor
 class Professor(BaseModel):
     nome: str
 
@@ -71,6 +72,5 @@ class Professor(BaseModel):
         from_attributes = True
 
 
-# Modelo de ENTRADA para criar professor.
 class ProfessorCreate(BaseModel):
     nome: str
--- models/estudante.py
+++ models/estudante.py
@@ -8,8 +8,11 @@ class Estudante(Base):
     __tablename__ = "estudantes"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String)
-    email = Column(String)
+    nome = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+
+    senha_hash = Column(String, nullable=False)
+    role = Column(String, nullable=False, default="estudante")
 
     perfil = relationship(
         "Perfil",
@@ -25,7 +28,4 @@ class Estudante(Base):
     )
 
     professor_id = Column(Integer, ForeignKey("professores.id"))
-    professor = relationship(
-        "Professor",
-        back_populates="estudantes",
-    )
+    professor = relationship("Professor", back_populates="estudantes")
