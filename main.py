01f20d70c636d703ad07575ee9ad1a3e15950699
dgusfr <diegogustavo.fr@gmail.com>
2026-05-28 15:33:29 -0300

    feat: add endpoint to retrieve item by ID

--- main.py
+++ main.py
@@ -19,3 +19,8 @@ async def greet_name(name: str):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
